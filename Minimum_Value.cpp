61add3f0633394410f15ae0ae16db48b7f3d0c51
Erza0838 <muhammaderza884@gmail.com>
2023-03-23 10:13:45 +0700

    Memperbaiki input

--- Minimum_Value.cpp
+++ Minimum_Value.cpp
@@ -2,17 +2,82 @@
 
 using namespace std;
 
-int main()
-{  
+// Function untuk memasukkan input
+void Input()
+{   
     // Array untuk menyimpan input
-    int inputNumbers = {};
+    int inputNumbers[5] = {};
 
-    cout << "Masukkan angka : " << endl;
-    cin >> inputNumbers;
-
-    if(inputNumbers + 1 < 0)
+    if(inputNumbers[0] < 1 || inputNumbers[0] == 0)
     {
-        cout << " Nilai paling kecil adalah : " << inputNumbers << endl;
+        cout << "Masukkan angka ke 0 : " << endl;
+        cin >> inputNumbers[0];
+        cout << "Angka ke 0 : " <<  inputNumbers[0] << endl;
     }
+
+    if(inputNumbers[1] < 1 || inputNumbers[1] == 0)
+    {
+        cout << "Masukkan angka ke 1 : " << endl;
+        cin >> inputNumbers[1];
+        cout << "Angka ke 1 : " << inputNumbers[1] << endl;
+    }
+
+    if(inputNumbers[2] < 1 || inputNumbers[2] == 0)
+    {
+        cout << "Masukkan angka ke 2 : " << endl;
+        cin >> inputNumbers[2];
+        cout << "Angka ke 2 : " << inputNumbers[2] << endl;
+    }
+
+    if(inputNumbers[3] < 1 || inputNumbers[3] == 0)
+    {
+        cout << "Masukkan angka ke 3 : " << endl;
+        cin >> inputNumbers[3];
+        cout << "Angka ke 3 : " << inputNumbers[3] << endl;
+    }
+
+    if(inputNumbers[4] < 1 || inputNumbers[4] == 0)
+    {
+        cout << "Masukkan angka ke 4 : " << endl;
+        cin >> inputNumbers[4];
+        cout << "Angka ke 4 : " << inputNumbers[4] << endl;
+    }
+
+    if(size_t(inputNumbers) > 0)
+    {
+        cout << "Array tidak kosong" << endl;
+        for(int compareValue = 0; compareValue < size_t(inputNumbers); compareValue++)
+        {
+            if(inputNumbers[compareValue] + 1 < compareValue || inputNumbers[compareValue] * 1 < compareValue)
+            {
+                cout << "Nilai minimumnya adalah : " << inputNumbers[compareValue] << endl;
+                break;
+            }
+        }
+    }
+}
+// Baris akhir function 
+
+// Function menampilkan nilai paling kecil
+// int MinimumInputValue(int arrayValue)
+// {
+//     for(int compareValue = 0; compareValue < arrayValue; compareValue++)
+//     {
+//         if(arrayValue[compareValue] + 1 < arrayValue[compareValue] || arrayValue[compareValue] * 1 < arrayValue[compareValue])
+//         {
+//             return arrayValue[compareValue];
+//         }
+//     }
+// }
+// Baris akhir function
+
+int main()
+{   
+    // Function untuk memasukkan input
+    Input();
+    // Baris akhir function 
+
+    // Menampilkan nilai paling kecil
+    // cout << "Nilai minimumnya adalah : " << MinimumInputValue(inputNumbers) << endl;
     return 0;
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+inputNumbers[5] = {}
